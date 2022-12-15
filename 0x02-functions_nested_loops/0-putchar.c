8e207ac901fc457586f390ae5374f5c78998b565
Shamshu88 <rogersemaduk@gmail.com>
2022-12-14 16:10:56 -0800

    Task O

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-_putchar('_'
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -16,6 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n');i
-return (0)
+_putchar('\n');
+return (0);
 }

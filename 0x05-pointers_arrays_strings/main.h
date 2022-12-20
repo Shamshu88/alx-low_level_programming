34f5a268e9113304d0b23dce4b47010c4f5ddef1
Shamshu88 <rogersemaduk@gmail.com>
2022-12-19 16:13:45 -0800

    more

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n)
-int _strlen(char *s)
-void swap_int(int *a, int *b)
-void _puts(char *str)
+void reset_to_98(int *n);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void _puts(char *str);
 
+	
 #endif

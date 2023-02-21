e06a42478ac9c7095a32898854d1a121ecd26e0a
Shamshu88 <rogersemaduk@gmail.com>
2023-02-21 08:22:18 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 9
- * Return: The numbers since 0 up to 9
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
-
 void print_numbers(void)
 {
+	int c;
 
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
+	for (c = 48; c < 58; c++)
 	{
 	_putchar(c);
 	}

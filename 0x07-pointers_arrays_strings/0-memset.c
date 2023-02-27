82611fce2103cb001cf43c8a0a2bb2db46446a24
Shamshu88 <rogersemaduk@gmail.com>
2023-02-27 08:32:58 -0800

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
- *_memset - fills memory with a constant byte
+ *_memset - fill a block of memory with a specific value
+ *@s: starting address of memory to be filled
+ *@b: the desired value
+ *@n: number of bytes to be changed
  *
- * @s: string
- * @n: an integer
- * @b: a character
- *
- *Return: a string
+ *Return: changed array with new value for n bytes
  */
-
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	while (i < n)
+	for (; n > 0; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
+		n--;
 	}
-
 	return (s);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,18 @@
 #include "main.h"
-
 /**
- *strchr -> string character
- *@s: string given
- *@c: another char
- *Return: a string
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
-	int a = 0, b;
+	int i = 0;
 
-	while (s[a])
-		a++;
-	for (b = 0; b < a; b++)
+	for (; s[i] >= '\0'; i++)
 	{
-		if (c == s[b])
-			s += b;
-		return (s);
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	return ('\0');
+	return (0);
 }
